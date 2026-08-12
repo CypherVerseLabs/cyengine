167f5edda6b866614ec672c00eb225662a573ca2
CypherVerseLabs <1cyphercoin@gmail.com>
2026-08-12 14:09:12 -0500

    Update branding and links

--- examples/pages/_app.tsx
+++ examples/pages/_app.tsx
@@ -30,24 +30,24 @@ function MyApp({ Component, pageProps }: AppProps) {
           property="og:description"
           content="An npm library that provides a standardized reality for the future of the 3D Web."
         />
-        <meta property="og:image" content="/android-chrome-512x512.png" />
+        <meta property="og:image" content="/android-chrome-512x512.ico" />
         {/* icons */}
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/apple-touch-icon.png"
+          href="/apple-touch-icon.ico"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon-32x32.png"
+          href="/favicon-32x32.ico"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicon-16x16.png"
+          href="/favicon-16x16.ico"
         />
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="msapplication-TileColor" content="#da532c" />
